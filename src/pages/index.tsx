b1142402532c6844fc5860eb31db5a0684b1abf0
taichi sugie <admin@sugietaichi.com>
2022-12-12 00:15:27 +0000

    lint prettie追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Copyright from '@components/Copyright'
+
+export default function Home() {
+  return (
+    <>
+      <Typography>index</Typography>
+      <Copyright />
+    </>
+  )
+}
