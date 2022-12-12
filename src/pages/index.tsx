95c3f3a923cebed51ea536582b4c33ea6959359b
sugietacihi <dev@sugietaichi.com>
2022-12-12 01:30:22 +0000

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Copyright } from '@mui/icons-material'
 import { Typography } from '@mui/material'
+import Copyright from '@components/Copyright'
 
 export default function Home() {
   return (
     <>
-      <Typography>{process.env.NEXT_PUBLIC_KEY1}</Typography>
+      <Typography>{process.env.NEXT_PUBLIC_KEY}</Typography>
 
       <Copyright />
     </>
