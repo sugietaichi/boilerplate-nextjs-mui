ede54c3acc01379d6b118275cf1ba190fc9d005a
sugietacihi <dev@sugietaichi.com>
2022-12-12 01:58:32 +0000

    firebase setting 追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Typography } from '@mui/material'
 import Copyright from '@components/Copyright'
+import { useEffect } from 'react'
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from '@src/utilities/firebase'
 
 export default function Home() {
+  useEffect(() => {}, [])
   return (
     <>
-      <Typography>{process.env.NEXT_PUBLIC_KEY}</Typography>
-
       <Copyright />
     </>
   )
